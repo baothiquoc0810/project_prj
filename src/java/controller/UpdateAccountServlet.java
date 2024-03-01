4a2a66cb973eb91dfa2c4e7a2a62a6107142c9ba
baothiquoc <bquoc3002@gmail.com>
2024-03-01 18:54:34 +0700

    update infomation

--- src/java/controller/UpdateAccountServlet.java
+++ src/java/controller/UpdateAccountServlet.java
@@ -4,6 +4,7 @@
  */
 package controller;
 
+import dal.DAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -11,13 +12,15 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import modal.Users;
 
 /**
  *
  * @author bquoc
  */
-@WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
-public class LoginServlet extends HttpServlet {
+@WebServlet(name = "UpdateAccountServlet", urlPatterns = {"/update"})
+public class UpdateAccountServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -36,10 +39,10 @@ public class LoginServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");
+            out.println("<title>Servlet UpdateAccountServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet UpdateAccountServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -57,8 +60,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-        response.getWriter().print("baothiquoc");
+        request.getRequestDispatcher("/WEB-INF/views/updateAccount.jsp").forward(request, response);
     }
 
     /**
@@ -72,11 +74,48 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String username_raw = request.getParameter("email");
-        String pass_raw = request.getParameter("password");
+        String displayName = request.getParameter("displayName");
+        String username = request.getParameter("username");
+        String oldPassword = request.getParameter("oldPassword");
+        String newPassword = request.getParameter("newPassword");
+        String confirmPassword = request.getParameter("confirmPassword");
 
-        response.getWriter().print(username_raw + ", " + pass_raw);
+        DAO d = new DAO();
+        response.getWriter().print(displayName);
+        response.getWriter().print(username);
+        boolean b = d.checkPass(oldPassword, username);
+        if (oldPassword.equals("")) {
+            d.updateDisplayName(username, displayName);
+            Users u = d.getUserByUsername(username);
+            HttpSession session = request.getSession();
+            session.setAttribute("account", u);
+            response.sendRedirect("home");
+        } else {
+            if (!confirmPassword.equals(newPassword)) {
+                request.setAttribute("error", "Password does not the same");
+                request.getRequestDispatcher("/WEB-INF/views/updateAccount.jsp").forward(request, response);
+            } else {
+                if (!b) {
+                    request.setAttribute("error", "Old password is incorrect");
+                    request.getRequestDispatcher("/WEB-INF/views/updateAccount.jsp").forward(request, response);
+                } else {
+                    d.updatePassword(newPassword, username);
+                    d.updateDisplayName(username, displayName);
+                    Users u = d.getUserByUsername(username);
+                    HttpSession session = request.getSession();
+                    session.setAttribute("account", u);
+                    response.sendRedirect("home");
+                }
+            }
+        }
     }
+//void if oldPassword is null then update displayname
+    //void if oldPassword is not null then check if oldPassword is correct then update password
+// public void updateAccount(String displayName, String username, String oldPassword, String newPassword, String confirmPassword){
+//     DAO d = new DAO();
+//     if(oldPassword == null){
+//         d.updateDisplayName(displayName, 1);
+//     }else{
 
     /**
      * Returns a short description of the servlet.
